2a1e506cf4014178554916accdacc462d812e588
3xt3r <47951705+3xt3r@users.noreply.github.com>
2026-06-09 00:16:03 +0300

    Update init_updater.py

--- init_updater.py
+++ init_updater.py
@@ -1,3 +1,4 @@
-sudo docker exec dependency-track-apiserver-1 \
+ sudo docker exec dependency-track-apiserver-1 \
   cat /tmp/Dependency-Track-0_0_0_0-8080/webapp/WEB-INF/classes/application.properties | \
   grep -iE "osv|github|nvd|epss|google"
+(venv) kali@kali-RedmiBook-16:~/Desktop/oss_checks$ 
