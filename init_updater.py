9fcb7434de4a03343f48425552ecff9b2198a9bd
3xt3r <47951705+3xt3r@users.noreply.github.com>
2026-06-09 00:15:44 +0300

    Update init_updater.py

--- init_updater.py
+++ init_updater.py
@@ -1,13 +1,3 @@
 sudo docker exec dependency-track-apiserver-1 \
-  find / -name "application.properties" 2>/dev/null | head -5
-[sudo] password for kali: 
-/tmp/Dependency-Track-0_0_0_0-8080/webapp/WEB-INF/classes/application.properties
-(venv) kali@kali-RedmiBook-16:~/Desktop/oss_checks$ sudo docker exec dependency-track-apiserver-1 \
-  env | sort | grep -iE "osv|github|nvd|epss|google|mirror"
-EPSS_ENABLED=true
-GITHUB_ADVISORIES_ACCESS_TOKEN=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
-GITHUB_ADVISORIES_ENABLED=true
-GOOGLE_OSV_ENABLED=PyPI;npm;Maven;crates.io;NuGet;RubyGems;Go;Hex;Pub;Packagist
-NVD_API_ENABLED=true
-NVD_API_KEY=31DACAE2-4941-F111-836A-129478FCB64D
-(venv) kali@kali-RedmiBook-16:~/Desktop/oss_checks$ 
+  cat /tmp/Dependency-Track-0_0_0_0-8080/webapp/WEB-INF/classes/application.properties | \
+  grep -iE "osv|github|nvd|epss|google"
