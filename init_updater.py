c91617448818f691baae2feb014060f73116d8f2
3xt3r <47951705+3xt3r@users.noreply.github.com>
2026-06-08 23:30:50 +0300

    Update init_updater.py

--- init_updater.py
+++ init_updater.py
@@ -1,7 +1,8 @@
-curl -s http://localhost:8080/api/v1/metrics/vulnerability \
-  -H "X-Api-Key: $ADMIN_KEY" | jq '{
+url -s http://localhost:8080/api/v1/metrics/vulnerability \
+  -H "X-Api-Key: $odt_WeFtwiB1_NVGyddcyg2GQTyOPHeQit6qMBNFnwxJM" | jq '{
     nvd:      .nvd,
     github:   .github,
     osv:      .osv,
     internal: .internal
   }'
+jq: parse error: Invalid numeric literal at line 1, column 10
