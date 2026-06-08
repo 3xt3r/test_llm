562b5331cb670e826e6b7cfaa1bdfa896aa224a6
3xt3r <47951705+3xt3r@users.noreply.github.com>
2026-06-09 00:20:25 +0300

    Update init_updater.py

--- init_updater.py
+++ init_updater.py
@@ -1,4 +1,7 @@
- sudo docker exec dependency-track-apiserver-1 \
-  cat /tmp/Dependency-Track-0_0_0_0-8080/webapp/WEB-INF/classes/application.properties | \
-  grep -iE "osv|github|nvd|epss|google"
-(venv) kali@kali-RedmiBook-16:~/Desktop/oss_checks$ 
+Попробуем найти где описаны настройки OSV:
+bashsudo docker exec dependency-track-apiserver-1 \
+  grep -r "osv\|OSV\|google.osv" /tmp/Dependency-Track-0_0_0_0-8080/ 2>/dev/null | \
+  grep -v ".class" | head -20
+И посмотрим полный application.properties:
+bashsudo docker exec dependency-track-apiserver-1 \
+  cat /tmp/Dependency-Track-0_0_0_0-8080/webapp/WEB-INF/classes/application.properties
