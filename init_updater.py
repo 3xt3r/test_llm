fe6eae4605cdc231dc65017a90442eb1d17af6f3
3xt3r <47951705+3xt3r@users.noreply.github.com>
2026-06-08 23:33:04 +0300

    Update init_updater.py

--- init_updater.py
+++ init_updater.py
@@ -1,4 +1,4 @@
-curl -s "http://localhost:8081/api/v1/vulnerability?pageSize=1&source=NVD" \
+(venv) kali@kali-RedmiBook-16:~/Desktop/oss_checks$ curl -s "http://localhost:8081/api/v1/vulnerability?pageSize=1&source=NVD" \
   -H "X-Api-Key: odt_WeFtwiB1_NVGyddcyg2GQTyOPHeQit6qMBNFnwxJM" \
   -I | grep -i "x-total-count"
 
@@ -9,3 +9,12 @@ curl -s "http://localhost:8081/api/v1/vulnerability?pageSize=1&source=GITHUB" \
 curl -s "http://localhost:8081/api/v1/vulnerability?pageSize=1&source=OSV" \
   -H "X-Api-Key: odt_WeFtwiB1_NVGyddcyg2GQTyOPHeQit6qMBNFnwxJM" \
   -I | grep -i "x-total-count"
+X-Total-Count: 355986
+Access-Control-Allow-Headers: Origin, Content-Type, Authorization, X-Requested-With, Content-Length, Accept, Origin, X-Api-Key, X-Total-Count, *
+Access-Control-Expose-Headers: Origin, Content-Type, Authorization, X-Requested-With, Content-Length, Accept, Origin, X-Api-Key, X-Total-Count
+X-Total-Count: 355986
+Access-Control-Allow-Headers: Origin, Content-Type, Authorization, X-Requested-With, Content-Length, Accept, Origin, X-Api-Key, X-Total-Count, *
+Access-Control-Expose-Headers: Origin, Content-Type, Authorization, X-Requested-With, Content-Length, Accept, Origin, X-Api-Key, X-Total-Count
+X-Total-Count: 355986
+Access-Control-Allow-Headers: Origin, Content-Type, Authorization, X-Requested-With, Content-Length, Accept, Origin, X-Api-Key, X-Total-Count, *
+Access-Control-Expose-Headers: Origin, Content-Type, Authorization, X-Requested-With, Content-Length, Accept, Origin, X-Api-Key, X-Total-Count
